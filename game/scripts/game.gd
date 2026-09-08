c205ac5fb07016455bf38538342aad71961876a9
Shaverian <jonah.parness@gmail.com>
2026-09-08 11:45:42 -0400

    Added red slime
    Added red slime, will eventually be used to test enemies that fire bullets. (may have broken stuff)

--- game/scripts/game.gd
+++ game/scripts/game.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
 
-func spawn_mob():
+func spawn_mob1():
 	%PathFollow2D.progress_ratio = randf()
-	var new_mob = preload("res://scenes/mob.tscn").instantiate()
+	var new_mob = preload("res://scenes/mob1.tscn").instantiate()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+func spawn_mob2():
+	%PathFollow2D.progress_ratio = randf()
+	var new_mob = preload("res://scenes/mob2.tscn").instantiate()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
 
 func _on_timer_timeout():
-	spawn_mob()
+	spawn_mob1()
+	spawn_mob2()
 
 
 func _on_player_health_depleted():
