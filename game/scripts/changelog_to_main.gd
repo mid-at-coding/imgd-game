fd63b65b3a98d199b92db4aee43ca64b26f23895
EpiCatz <epicoolcatz@gmail.com>
2026-09-14 22:57:35 -0400

    Added Back Button Asset
    Added back button asset to changelog and credits

--- game/scripts/changelog_to_main.gd
+++ game/scripts/changelog_to_main.gd
@@ -8,3 +8,7 @@ extends Button
 # Change scene
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
